720c0c2f4771e75dbc0f29e68932a3d7388750d8
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-03-20 11:50:36 +0900

    add: arrange view 추가

---
+++ App-Dev-Training/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by SHIN YOON AH on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
